f7a827f17bf8ed47a006c6e8e29d87edc514092b
Rene B. Dena <renedena842@gmail.com>
2022-04-11 18:48:08 -0700

    Stable

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ const baseUri = "ipfs://Qma3wVJFER2zSyoyty5AusKHzStw48NJCcTnGZmz8idGin";
 
 const solanaMetadata = {
   symbol: "MOM",
-  seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
+  seller_fee_basis_points: 10, // Define how much % you want from secondary market sales 1000 = 10%
   external_url: "https://www.youtube.com/c/hashlipsnft",
   creators: [
     {
