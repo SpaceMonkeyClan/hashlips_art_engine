51a0426fc3572203cb90f75153358a6950fd353f
Rene B. Dena <renedena842@gmail.com>
2022-04-14 16:48:34 -0700

    Stable

--- src/config.js
+++ src/config.js
@@ -5,9 +5,9 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "Mellow Fellow Felix";
-const description = "The cat on whom humans all gave up, the collection Mellow Fellow Felix depicts the everyday life of Miss Felix, who is a fluffy cute Norwegian Forest Cat. Despite her cuteness overloaded attributes, do not forget she is a forest cat afterall. She hunts with Hammers, Dual axes, Sword and shield too. After a hectic day at the forest hunting, she ends her day with a tummy full of yummy lip-smacking food. She’s often caught having pizza and doughnut, which is her 2nd and 3rd fav food after fish. To improve her efficiency as a hunter in the forest she often has to go undercover to blend with the nature so as her enemies cannot locate her. Miss Felix loves her black outfit, which makes her feel like a secret warrior. On a very rare day, when her mood is at the peak, she can be seen splashing in rainbow colours too. It was a very hard task to manage an interview with Miss Felix, and ask her all the details. Finally, after months of struggle to locate her, we have finally been lucky to get an interview from her, gathering all the details which we have crafted in 10000 NFTs. She has been very polite, and also promised us another round of interview with her aunt, who is a Lynx Cat. This will be featured in season 2.";
-const baseUri = "ipfs://QmWehH6cUnpo8Sdxh9eqNQzx4j3XrRLfh8oc989pF7cMue";
+const namePrefix = "Bored Baby";
+const description = "Bored Baby is a collection of 10,000 unique babies united together on the Polygon Blockchain.";
+const baseUri = "ipfs://QmYAsU85Ao69RDDMj5xUyYujLPYvucUpKyicQZyRcQGLVR";
 
 const solanaMetadata = {
   symbol: "MOM",
@@ -24,7 +24,7 @@ const solanaMetadata = {
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 10000,
+    growEditionSizeTo: 5,
     layersOrder: [
       { name: "Background" },
       { name: "Skin" },
