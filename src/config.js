d21feccc96ec2b1705f1f7a0dacebe4c7f41b342
Rene B. Dena <renedena842@gmail.com>
2022-05-19 21:57:10 -0700

    Stable

--- src/config.js
+++ src/config.js
@@ -6,8 +6,8 @@ const network = NETWORK.eth;
 
 // General metadata for Ethereum
 const namePrefix = "Goofy Gophers";
-const description = "10,000 Goofy Gophers have all gathered on the Polygon Blockchain to wreak havoc and pull pranks. It’s your job to put an end to the madness. Find and adopt your very own Goofy Gopher today. Only on OpenSea";
-const baseUri = "ipfs://REPLACE-WITH-CID";
+const description = "10,000 Goofy Gophers have all gathered on the Polygon Blockchain to wreak havoc and pull pranks. It's your job to put an end to the madness. Find and adopt your very own Goofy Gopher today. Only on OpenSea";
+const baseUri = "ipfs://Qmbfn7CexMEWc8rUCjFT51ro4hpSJENVpPc1bMxJKYrjV9";
 
 const solanaMetadata = {
   symbol: "MOM",
